d38183b87b98818f8cd46a7459ad793126e226bb
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-02-24 10:12:35 +0300

    commit X

--- app/assets/javascripts/discourse/app/components/glimmer/d-button.js
+++ app/assets/javascripts/discourse/app/components/glimmer/d-button.js
@@ -1,16 +1,26 @@
 import GlimmerComponent from "discourse/components/glimmer";
 
 export default class DButton extends GlimmerComponent {
-  type = "button";
-  form = null;
-  isLoading = false;
-  icon = null;
-  title = null;
-  translatedTitle = null;
-  label = null;
-  translatedLabel = null;
-  translatedAriaLabel = null;
-  ariaExpanded = null;
+  get className() {
+    const classes = ["btn"];
+    const hasText = !!this.args.label;
+    if (!hasText) {
+      classes.push("no-text");
+    }
+    if (this.args.icon) {
+      if (hasText) {
+        classes.push("btn-icon-text");
+      } else {
+        classes.push("btn-icon");
+      }
+    } else if (hasText) {
+      classes.push("btn-text");
+    }
 
-  className() {}
+    return classes.join(" ");
+  }
+
+  get icon() {
+
+  }
 }
